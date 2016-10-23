d91ade34868779ac00f37451d7ba018a85ea09fa
kensou714 <kensou714@qq.com>
2016-10-23 14:50:06 +0800

    修复选人PATCH，以及增加10个主角的指针信息

---
+++ src/game/Role/role_2/role_info_2.c
@@ -0,0 +1,44 @@
+/*
+		RoleInfoAddr = 0x10206F88
+*/
+#include "../../Include/Role.h"
+const Role Role_2_0={
+				0,			//short ROLE_FLAG
+				2,			//short ROLE_ID
+				0,			//int NULL
+				0,			//int NULL
+				0x10206E18,		//int ActionPtr
+				0x00000000,		//int 
+				0x10206DB0,		//int pImageIdTbl
+				0x101EAC60,		//int pPalTbl
+	(InputElem*)0x10206E48,		//int pInputTbl
+				0x104619FC,		//int pImageTbl
+				0x10205150,		//int pImageAxisTbl
+				0x1020568C,		//int pCollIndexTbl
+				0x10205BC8,		//int pCollTbl
+				0x002DABB6,		//int pBrom
+				0x01207B81,		//int pArom
+				354,			//int DeathSound1
+				355,			//int DeathSound2
+				353,			//int DeathSound3
+				352,			//int DeathSound4
+				0x00000000,		//int Null
+				0x00000000,		//int Null
+				0x00000000,		//int Null
+				0x00030000,		//int Null
+				0x00000001,		//int Null
+				0x00000000,		//int Null
+				0x10206F7C,		//int pSkillCancleTbl
+				0x00000000,		//int Null
+				0x00000000,		//int Null
+				0x00000000,		//int Null
+				0x00000000,		//int Null
+				0x00000000,		//int Null
+				0x00000000,		//int Null
+				0x00000000,		//int Null
+				0x00000000,		//int Null
+				0x00000013,		//int OrgWeaponId
+				0x101EB210,		//int pWeaponImageTbl
+				0x101EB4E8,		//int pWeaponImageAxisTbl
+				0x101EB7C0,		//int pWeaponLayerAttr
+};
--- src/game/System/PlayerSelect.c
+++ src/game/System/PlayerSelect.c
@@ -2,7 +2,7 @@
 #include "../game_type.h"
 #include "PlayerSelect.h"
 #include "../Include/Role.h"
-
+#include "../xyj2_func.h"
 
 extern Role JinJiao_0;
 extern Role YinJiao_0;
@@ -10,11 +10,12 @@ extern Role BaiGuJing_0;
 extern Role HuangPao_0;
 extern Role HuangFeng_0;
 extern Role HuangMei_0;
+extern Role	Role_0_0;
 
 //定义新选人指针表，扩展新人。还需要HOOK和PATCH多段函数配合  默认总人数暂定为30个
 Role* RolePtrTbl[MAX_ROLE_NUM] ={
 		(Role*)0x101EBA40,			//原有可选，用ROM指针必须强制转换为ROLE*类型
-		(Role*)0x101F8164,
+		(Role*)0x101EBA40,
 		(Role*)0x10206F88,
 		(Role*)0x10211EE8,
 		(Role*)0x1021DE14,
@@ -24,7 +25,7 @@ Role* RolePtrTbl[MAX_ROLE_NUM] ={
 		(Role*)0x1024FA20,
 		(Role*)0x1025C420,
 //添加人物   注意前面必须用&取指针，否则编译错误
-		&JinJiao_0,
+		&Role_0_0,
 		&YinJiao_0,
 		&BaiGuJing_0,
 		&HuangPao_0,
@@ -75,9 +76,80 @@ int InitSameRolePalNo()
 	int bPlayerNo; // r0@1
 
 	for ( bPlayerNo = 0; bPlayerNo < 4; bPlayerNo = (bPlayerNo + 1) & 0xFF )
-		P8(0x20057A84)[bPlayerNo] = 0;
+		P32(0x20057A84)[bPlayerNo] = 0;
 	return bPlayerNo;
 }
+
+int hook_sub_1002993A(int bPlayerNo)//修复人物颜色
+{
+  if ( P32(0x20057A84)[bPlayerNo] )
+    sub_100043D6(P32(0x20057A84)[bPlayerNo], RolePtrTbl[bPlayerNo]->pPalTbl);
+  else
+    P32(0x20057A84)[bPlayerNo] = rpPalette_FadeLoad(2, (bPlayerNo + 12) & 0xFF, RolePtrTbl[bPlayerNo]->pPalTbl, 32);
+  return P32(0x20057A84)[bPlayerNo];
+}
+
+int hook_sub_1002998C()
+{
+  int i; // r4@1
+  int result; // r0@4
+
+  for ( i = 0; i < 4; i = (i + 1) & 0xFF )
+  {
+    if ( P32(0x20057A84)[i] )
+    {
+      sub_10004376(P32(0x20057A84)[i]);
+      P32(0x20057A84)[i] = 0;
+    }
+    result = i + 1;
+  }
+  return result;
+}
+
+
+
+signed int hook_sub_100299C0()
+{
+  signed int v0; // r4@1
+  void *v1; // r5@1
+  signed int i; // r4@4
+  signed int result; // r0@7
+
+  sub_10019198();
+  sub_10029920();
+  sub_1002258C();
+  v0 = 0;
+  v1 = P32(0x20056BD4);
+  pgm2log("1");
+  while ( v0 < 4 )
+  {
+    V8(v1 + 6*2) = 0;
+    V8(v1 + 7*2) = v0;
+    V32(v1 + 30*4) = 0x20057744 + 192 * v0;
+    V32(v1 + 31*4) = 0x20057A44 + 16 * v0;//人物阴影
+    V32(v1 + 346*2) = 0;
+    V8(v1 + 359*2) = 255;
+    V8(v1 + 178*4) = 0;
+    V8(v1 + 358*2) = 0;
+    V8(v1 + 364*2) = 0;
+    ++v0;
+    v1 = v1 + 732;
+  }
+  V8(0x200200A9) = 0;
+  V8(0x200200AB) = 1;
+  V8(0x200200C7) = 1;
+  V16(0x200200AC) = 0;
+  V16(0x200200AE) = 0;
+  V8(0x200200B0) = 0;
+  V16(0x200200C4) = 0;
+  for ( i = 0; i < 10; ++i )
+    P32(0x20057A94)[i] = P32(0x1012EFB4)[i];
+  result = 10;
+  V8(0x200200AA) = 10;
+  return result;
+}
+
+
 
 //获得游戏区域 该函数后期应移动到全局函数文件
 int GetGameArea()

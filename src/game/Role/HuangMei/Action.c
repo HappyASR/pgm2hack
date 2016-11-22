68aa7741e9daa2ac6b5382c0da2a1e475916bd21
StevenWangZero <wuyuesisi@163.com>
2016-11-23 01:01:41 +0800

    修正黄眉ACTION.C的错误。

---
+++ src/game/Role/HuangMei/Action.c
@@ -0,0 +1,35 @@
+#include "../../Include/Action.h"
+
+extern const ActElem Act_HuangMei_Start[];
+const ActTypePtrTbl Role_HuangMei_ActPtrTbl_0 ={
+	0,
+	Act_HuangMei_Start,
+	(ActElem*)0x102C7440,
+	(ActElem*)0x102C7BC8,
+	(ActElem*)0x102C8A04,
+	(ActElem*)0x102C8A7C,
+	(ActElem*)0x102C8914,
+	0,
+	(ActElem*)0x102C75C0,
+	(ActElem*)0x102C7308,
+	0,
+	0
+};
+
+const ActTypePtrTbl const Role_HuangMei_ActPtrTbl_1 ={
+	0,
+	Act_HuangMei_Start,
+	(ActElem*)0x102C7440,
+	(ActElem*)0x102C7BC8,
+	(ActElem*)0x102C8A04,
+	(ActElem*)0x102C8A7C,
+	(ActElem*)0x102C8914,
+	0,
+	(ActElem*)0x102C75C0,
+	(ActElem*)0x102C7308,
+	0,
+	0
+};
+
+
+
